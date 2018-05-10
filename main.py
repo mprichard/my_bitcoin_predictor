af1576b3e61f9508d7b71211513f9fd7f3430fb0
Michael J. Prichard <michael@metismachine.com>
2018-05-10 11:23:52 -0400

    google hello

--- main.py
+++ main.py
@@ -183,7 +183,7 @@ hidden = model.init_hidden()
 
 # Train the model on 500 epochs
 # Ideally this number is tuned precisely
-NUM_EPOCHS = 50 
+NUM_EPOCHS = 80 
 for i in range(NUM_EPOCHS):
     def closure():
         model.zero_grad()
